3da16738186706a26c5dfa866ca1559fc4551490
Gion <1033499391@qq.com>
2026-07-04 14:33:39 +0800

    更新Readme
    更新了仓库的布局

--- EEG Classification Codes/PRPL-main/feature_wrap.m
+++
@@ -1,7 +0,0 @@
-function feature_2d=feature_wrap(feature_3d)
-    feature_2d=zeros(size(feature_3d,2),310);
-    for i=1:size(feature_3d,2)
-         feature_2d(i,:)=reshape(feature_3d(:,i,:),1,310);
-    end
-end
-         
